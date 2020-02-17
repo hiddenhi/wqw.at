1c154265c542238b8526bae6c2008f69ff30b234
长欧 <36437566+hiddenhi@users.noreply.github.com>
2020-02-17 18:24:40 +0800

    Update wqw2.js

--- wqw2.js
+++ wqw2.js
@@ -117,7 +117,7 @@ f.style.height="55px";
 f.style.margin="0 0 0 0";
 //document.body.
 
-parentNode.appendChild(f);
+this.parentNode.appendChild(f);
 var iframe = window.frames[d];
 iframe.document.open();
 iframe.document.write(base64.decode(base));
